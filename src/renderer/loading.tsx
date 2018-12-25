ea1b2ac8c07f301d7574e61c23c61afcbc4f4a5c
hakuginn220 <hakuginn220@white-falls.com>
2018-12-25 10:57:11 +0900

    update loading

--- src/renderer/loading.tsx
+++ src/renderer/loading.tsx
@@ -1,5 +1,38 @@
 import React, { SFC } from 'react'
+import styled, { keyframes } from 'styled-components'
 
-const Loading: SFC<{}> = () => <>loading...</>
+const animation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const LoadingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const LoadingCircle = styled.div`
+  width: 32px;
+  height: 32px;
+  border: 4px solid rgba(0, 0, 0, 0.2);
+  border-left: 4px solid rgba(0, 0, 0, 0.1);
+  border-radius: 50%;
+  text-indent: 100%;
+  white-space: nowrap;
+  overflow: hidden;
+  animation: ${animation} 1s infinite linear;
+`
+
+const Loading: SFC<{}> = () => (
+  <LoadingContainer>
+    <LoadingCircle>loading...</LoadingCircle>
+  </LoadingContainer>
+)
 
 export default Loading
